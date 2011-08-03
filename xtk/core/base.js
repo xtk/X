71cf61d6449156fb830108dfdd2487390a3d5c63
Daniel Haehn <dh@randi2.de>
2011-08-03 10:42:30 -0400

    Added a general method print() on the X.base class. Other classes do not have to re-implement it.
    Also re-factored the code with appropriate comments as examples for new
    classes.

--- xtk/core/base.js
+++ xtk/core/base.js
@@ -1,24 +1,68 @@
+/*
+ * ${HEADER}
+ */
+
+// provides
 goog.provide('X.base');
 
+// requires
+
+/**
+ * The superclass class for all X.base-objects.
+ * 
+ * @constructor
+ */
 X.base = function() {
   
+  //
+  // class attributes
+  
+  // The className of this class.
   this._className = 'base';
   
 };
 
+/**
+ * Print the className of the current X.base-object.
+ * 
+ * @returns {string} The className of the current X.object.
+ */
 X.base.prototype.getClassName = function() {
   
   return this._className;
   
 };
 
+/**
+ * Print the className and all attributes of the current X.base-object.
+ * 
+ * @returns {string} A string representation of the current X.base-object.
+ */
 X.base.prototype.print = function() {
   
-  output = '= X =\n';
-  output += 'className: ' + this.getClassName() + '\n';
+  attributes = Object.keys(this);
+  attributesStringList = '';
+  className = '<unknown>';
+  
+  // loop through the attributes of a class
+  for ( var a in attributes) {
+    
+    aName = 'this.' + attributes[a];
+    aValue = eval(aName);
+    
+    // catch the className, since we want to display it differently
+    if (aName == 'this._className') {
+      className = aValue;
+    } else {
+      attributesStringList += aName + ': ' + aValue + '\n';
+    }
+    
+  }
+  
+  // build string output
+  output = '== X.' + className + ' ==\n';
+  output += attributesStringList;
   
   return output;
   
 };
-
-
--- xtk/visualization/renderer.js
+++ xtk/visualization/renderer.js
@@ -1,36 +1,77 @@
+/*
+ * ${HEADER}
+ */
 
+// provides
 goog.provide('X.renderer');
 
+// requires
 goog.require('X.base');
 
-X.renderer = function() {
+/**
+ * Create a renderer with the given width and height.
+ * 
+ * @param {number}
+ *          width The width of the renderer.
+ * @param {number}
+ *          height The height of the renderer.
+ * @constructor
+ * @extends {X.base}
+ */
+X.renderer = function(width, height) {
   
+  // call the standard constructor of X.base
   goog.base(this);
   
-  this._className = 'renderer';  
-  this._dimension = 2;
+  //
+  // class attributes
+  
+  // The className of this class.
+  this._className = 'renderer';
+  
+  // The dimension of this renderer.
+  this._dimension = -1;
+  
+  // The width of this renderer.
+  this._width = -1;
+  
+  // The height of this renderer.
+  this._height = -1;
   
 };
+// inherit from X.base
 goog.inherits(X.renderer, X.base);
 
-//
-//
+/**
+ * Get the dimension of this renderer.
+ * E.g. 2 for two-dimensional, 3 for three-dimensional.
+ * 
+ * @returns {number} The dimension of this renderer.
+ */
 X.renderer.prototype.getDimension = function() {
   
   return this._dimension;
   
 };
 
-
-//
-//
-X.renderer.prototype.print = function() {
-
-  output = goog.base(this, 'print');
-  output += 'dimension: ' + this.getDimension() + '\n';
+/**
+ * Get the width of this renderer.
+ * 
+ * @returns {number} The width of this renderer.
+ */
+X.renderer.prototype.getWidth = function() {
   
-  return output;
+  return this._width;
   
 };
 
-
+/**
+ * Get the height of this renderer.
+ * 
+ * @returns {number} The height of this renderer.
+ */
+X.renderer.prototype.getHeight = function() {
+  
+  return this._height;
+  
+};
