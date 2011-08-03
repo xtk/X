45d37023058a27b604aa1fe5c8ba40cab27315d2
Daniel Haehn <dh@randi2.de>
2011-08-03 06:37:06 -0400

    Forgot some untracked files from last commit.

--- xtk/xtk.js
+++ xtk/xtk.js
@@ -1,13 +1,4 @@
-
-//
+// entry point
 
 // namespace
 goog.provide('X');
-goog.provide('X.import');
-
-
-X.import = __importer;
-
-X.import('xtk/core/base.js');
-X.import('xtk/visualization/renderer.js');
-X.import('sampleApp.js');
