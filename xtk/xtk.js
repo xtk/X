2a8e1bff8b3cfbe6931880ca5682602ecf241354
Daniel Haehn <d@klavierfabrik.com>
2011-10-05 21:41:57 -0400

    Review X.base class.
    Also, introduce the X.uniqueId functionality which generates uniqueIds without using the goog.events.uniqueId call (which was a hack).

--- xtk/xtk.js
+++ xtk/xtk.js
@@ -2,3 +2,18 @@
 
 // namespace
 goog.provide('X');
+
+// uniqueId mechanism
+var __uniqueIdCounter = 0;
+
+/**
+ * Return a uniqueId with the given prefix.
+ * 
+ * @param {string} prefix A prefix for the unique id.
+ * @returns {string} A uniqueId.
+ */
+X.uniqueId = function(prefix) {
+
+  return prefix + __uniqueIdCounter++;
+  
+};
