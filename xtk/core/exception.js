87567301473c01082b99e428b60faceea81c8cb5
Daniel Haehn <d@klavierfabrik.com>
2011-10-05 21:48:56 -0400

    Review X.exception.
    Little re-factoring and adding comments..

--- xtk/core/exception.js
+++ xtk/core/exception.js
@@ -12,44 +12,61 @@ goog.require('X.base');
 
 /**
  * The general xtk exception.
- *
+ * 
  * @param {?string} message An error message.
  * @constructor
  * @extends {X.base}
  */
 X.exception = function(message) {
 
+  var validMessage = 'Unknown error!';
+  
+  if (goog.isDefAndNotNull(message)) {
+    
+    // use the given message
+    validMessage = message;
+    
+  }
+  
+  //
+  // call the standard constructor of X.base
   goog.base(this);
-
+  
   //
   // class attributes
-
+  
   /**
    * @inheritDoc
    * @const
    */
   this._className = 'exception';
-
-  // The message associated to this exception.
-  if (message) {
-    this._message = message;
-  } else {
-    this._message = 'Unknown error!';
-  }
-
-  // we will use a new error object to get the stacktrace
+  
+  /**
+   * The message of this exception.
+   * 
+   * @type {!string}
+   * @protected
+   */
+  this._message = validMessage;
+  
+  /**
+   * The associated error object of this exception.
+   * 
+   * @type {Error}
+   * @private
+   */
   var _err = new Error();
-
+  
   /**
    * The stack trace of this exception.
-   *
+   * 
    * @type {!string}
    * @private
    */
   this._stackTrace_ = _err.stack;
-
+  
 };
 goog.inherits(X.exception, X.base);
 
-// export symbols (requiered for advanced compilation)
+// export symbols (required for advanced compilation)
 goog.exportSymbol('X.exception', X.exception);
