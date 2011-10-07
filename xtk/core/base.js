1b0fae372e875e9ef252f76b21e1658803d68be0
Daniel Haehn <d@klavierfabrik.com>
2011-10-06 21:18:43 -0400

    Fix an un-initialization warning in X.base.

--- xtk/core/base.js
+++ xtk/core/base.js
@@ -15,7 +15,7 @@ goog.require('goog.events.EventTarget');
 /**
  * The superclass class for all X.base-objects. All derived objects will be
  * registered for event handling.
- *
+ * 
  * @constructor
  * @extends {goog.events.EventTarget}
  */
@@ -25,26 +25,26 @@ X.base = function() {
   // register this class within the event system by calling the superclass
   // constructor
   goog.base(this);
-
+  
   //
   // class attributes
-
+  
   /**
    * The className of this class.
-   *
+   * 
    * @type {string}
    * @protected
    */
   this._className = 'base';
-
+  
   /**
    * The uniqueId of this object. Each instance in XTK has a uniqueId.
-   *
+   * 
    * @type {string}
    * @protected
    */
   this._id = X.uniqueId('X');
-
+  
 };
 // enable events
 goog.inherits(X.base, goog.events.EventTarget);
@@ -52,31 +52,31 @@ goog.inherits(X.base, goog.events.EventTarget);
 
 /**
  * Print the className of the current X.base-object.
- *
+ * 
  * @return {string} The className of the current X.object.
  */
 X.base.prototype.className = function() {
 
   return this._className;
-
+  
 };
 
 
 /**
  * Return the unique id of the current X.base-object.
- *
+ * 
  * @return {string} The unique id of the current X.object.
  */
 X.base.prototype.id = function() {
 
   return this._id;
-
+  
 };
 
 
 /**
  * Print the className and all attributes of the current X.base-object.
- *
+ * 
  * @return {string} A string representation of the current X.base-object.
  */
 X.base.prototype.print = function() {
@@ -84,29 +84,29 @@ X.base.prototype.print = function() {
   var attributes = Object.keys(this);
   var attributesStringList = '';
   var className = '<unknown>';
-
+  
   // loop through the attributes of a class
-  var a;
+  var a = 0;
   for (a in attributes) {
-
+    
     var aName = 'this.' + attributes[a];
     var aValue = eval(aName);
-
+    
     // catch the className, since we want to display it differently
     if (aName == 'this._className') {
       className = aValue;
     } else {
       attributesStringList += aName + ': ' + aValue + '\n';
     }
-
+    
   }
-
+  
   // build string output
   var output = '== X.' + className + ' ==\n';
   output += attributesStringList;
-
+  
   return output;
-
+  
 };
 
 // export symbols (required for advanced compilation)
