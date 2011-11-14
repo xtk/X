497275da70c8d302941fae9d2c189935d6910881
Daniel Haehn <d@klavierfabrik.com>
2011-11-13 22:16:20 -0500

    Added workaround to support Safari and Opera Next.
    Browsers currently running sampleApp properly:
    
    1) Chrome
    2) Firefox > 4.0
    3) Safari > 5.1 with WebGL enabled in the developer toolbar (preferences -> advanced)
    4) Opera Next 12.0

--- xtk/xtk.js
+++ xtk/xtk.js
@@ -9,12 +9,45 @@ var __uniqueIdCounter = 0;
 
 /**
  * Return a uniqueId with the given prefix.
- *
+ * 
  * @param {string} prefix A prefix for the unique id.
  * @return {string} A uniqueId.
  */
 X.uniqueId = function(prefix) {
 
   return prefix + __uniqueIdCounter++;
-
+  
 };
+
+
+//
+// BROWSER COMPATIBILITY FIXES GO HERE
+//
+
+//
+// 1. Safari does not support the .bind(this) functionality which is crucial for
+// XTK's event mechanism. This hack fixes this.
+//
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError(
+          "Function.prototype.bind - what is trying to be bound is not callable");
+    }
+    
+    var fSlice = Array.prototype.slice, aArgs = fSlice.call(arguments, 1), fToBind = this, fNOP = function() {
+
+    }, fBound = function() {
+
+      return fToBind.apply(this instanceof fNOP ? this : oThis || window, aArgs
+          .concat(fSlice.call(arguments)));
+    };
+    
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+    
+    return fBound;
+  };
+}
