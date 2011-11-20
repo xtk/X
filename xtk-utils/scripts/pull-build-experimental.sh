c9b64f38450bf9e8c064e903fbc24cb8f0d01b39
Daniel <d@a1.klavierfabrik.com>
2011-11-20 16:40:43 -0500

    Rename cron scripts.

---
+++ xtk-utils/scripts/pull-build-experimental.sh
@@ -0,0 +1,10 @@
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+XTKUTILS_DIR=$SCRIPTS_DIR/../
+XTK_DIR=$XTKUTILS_DIR/../
+
+cd $XTK_DIR
+git checkout master
+git pull
+cd $XTKUTILS_DIR
+python build.py -e
+
