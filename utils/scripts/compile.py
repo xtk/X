3e1c2c2e1fc7db392b5c2bac2aff79ec2845d3eb
Daniel Haehn <d@klavierfabrik.com>
2011-12-11 20:31:35 -0500

    Update the compiler to ignore all non-source files.

--- utils/scripts/compile.py
+++ utils/scripts/compile.py
@@ -3,50 +3,50 @@
 import os, sys
 import time
 
-def calculate( namespace, dir, xtkdir, buildtool, compiler):
+def calculate( namespace, dir, xtkdir, buildtool, compiler ):
     print '+++++++++++++++++++++++++++++++'
     print 'Namespace: ' + namespace
     print 'Directory: ' + dir
     print 'Build Tool: ' + buildtool
-    excludeDirs = ['lib', 'testing', 'doc',  'css']
+    excludeDirs = ['lib', 'testing', 'doc', 'css', '.git', '.settings', 'utils']
     print 'Exclude Dirs: '
     print excludeDirs
-    excludeFiles = ['.html', '.txt', '-deps.js']
+    excludeFiles = ['.html', '.txt', '-deps.js', 'LICENSE', 'README', '.project', '.pydevproject', '.gitignore', '.DS_Store']
     print 'Exclude Files: '
     print excludeFiles
     output = dir + '-build'
     print 'Output: ' + output
     # create ouput folder if it doesn't exist
-    if not os.path.exists(output): os.makedirs(output)
+    if not os.path.exists( output ): os.makedirs( output )
 
     # remove file if exists
-    if os.path.exists('temp_build.log'): os.remove( 'temp_build.log' )
+    if os.path.exists( 'temp_build.log' ): os.remove( 'temp_build.log' )
 
     # start compilation time
     start = time.clock()
-    
+
     #compile all files
-    if(namespace == 'X'):
-        for root, dirs, files in os.walk(dir):
-            
-            removeDirs = list(set(excludeDirs) & set(dirs))
+    if( namespace == 'X' ):
+        for root, dirs, files in os.walk( dir ):
+
+            removeDirs = list( set( excludeDirs ) & set( dirs ) )
             for i in removeDirs:
-                dirs.remove(i)
-            
-            relative_path = os.path.relpath(root, dir)
+                dirs.remove( i )
+
+            relative_path = os.path.relpath( root, dir )
             real_output = output + os.sep + relative_path
-            if not os.path.exists(real_output): os.makedirs(real_output)
-            
+            if not os.path.exists( real_output ): os.makedirs( real_output )
+
             for name in files:
                 # check sanity of file
                 compile = True
                 for eName in excludeFiles:
                     if eName in name:
                         compile = False
-                        
+
                 if compile:
                     command = buildtool
-                    command += ' --input ' + os.path.join(root, name)
+                    command += ' --input ' + os.path.join( root, name )
                     command += ' --root=' + xtkdir
                     command += ' --output_mode=compiled'
                     command += ' --compiler_jar=' + compiler
@@ -61,8 +61,8 @@ def calculate( namespace, dir, xtkdir, buildtool, compiler):
                     # get current location
                     os.system( 'bash scripts/colorPrompt.sh temp' )
                     os.system( 'cat temp >> temp_build.log' )
-                    if os.path.exists('temp'): os.remove( 'temp' )
-    
+                    if os.path.exists( 'temp' ): os.remove( 'temp' )
+
     # compile project
     else:
         command = buildtool
@@ -81,22 +81,22 @@ def calculate( namespace, dir, xtkdir, buildtool, compiler):
         os.system( command + ' &> temp' )
         os.system( 'bash colorPrompt.sh temp' )
         os.system( 'cat temp >> temp_build.log' )
-        if os.path.exists('temp'): os.remove( 'temp' )
+        if os.path.exists( 'temp' ): os.remove( 'temp' )
 
     # end compilation time
     end = time.clock()
     processingTime = end - start
 
-    if(namespace == 'X'):
-        os.system( 'echo ' + str(start) + ' > xtk_build.log')
-        os.system( 'echo ' + str(end) + ' >> xtk_build.log')
-        os.system( 'echo ' + str(processingTime) + ' >> xtk_build.log')
-        os.system( 'cat temp_build.log >> xtk_build.log')
+    if( namespace == 'X' ):
+        os.system( 'echo ' + str( start ) + ' > xtk_build.log' )
+        os.system( 'echo ' + str( end ) + ' >> xtk_build.log' )
+        os.system( 'echo ' + str( processingTime ) + ' >> xtk_build.log' )
+        os.system( 'cat temp_build.log >> xtk_build.log' )
     else:
-        os.system( 'echo ' + str(start) + ' > project_build.log')
-        os.system( 'echo ' + str(end) + ' >> project_build.log')
-        os.system( 'echo ' + str(processingTime) + ' >> project_build.log')
-        os.system( 'cat temp_build.log >> project_build.log')
+        os.system( 'echo ' + str( start ) + ' > project_build.log' )
+        os.system( 'echo ' + str( end ) + ' >> project_build.log' )
+        os.system( 'echo ' + str( processingTime ) + ' >> project_build.log' )
+        os.system( 'cat temp_build.log >> project_build.log' )
 
-    if os.path.exists('temp_build.log'): os.remove( 'temp_build.log' )
+    if os.path.exists( 'temp_build.log' ): os.remove( 'temp_build.log' )
     print '>> OUTPUT: ' + output + os.sep + namespace + '.js'
