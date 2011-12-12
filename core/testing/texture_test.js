0c13b15394fdde7ae94cc766c22afd0de7ea1efa
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2011-12-12 15:51:09 -0500

    WIP: adding tests
    @haehn: which is the utility of image in texture.js
    @haehn: we might need a data repository to run the tests
    @haehn: tests should be run against uncompiled, compiled or both?

---
+++ core/testing/texture_test.js
@@ -0,0 +1,17 @@
+goog.require('X.base');
+goog.require('X.texture');
+goog.require('goog.testing.jsunit');
+
+// difference between image and file???
+/**
+ * Test for X.texture.getClassName
+ */
+//function testXtextureFile() {
+
+  //t = new X.texture();
+
+  //assertEquals(t.className(), 'texture');
+
+//}
+
+
