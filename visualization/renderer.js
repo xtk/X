1ba16f7327b9195c70d664b71ef35a5fcc4280ee
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-01-12 17:26:02 -0500

    Bump up version number.

--- visualization/renderer.js
+++ visualization/renderer.js
@@ -281,7 +281,7 @@ X.renderer = function(container) {
    */
   this._locked = false;
   
-  window.console.log('XTK Release 0 -- http://www.goXTK.com');
+  window.console.log('XTK Release 1 -- 1/12/12 -- http://www.goXTK.com');
 };
 // inherit from X.base
 goog.inherits(X.renderer, X.base);
