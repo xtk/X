f43d4484aa26162832a99eb45095c618eba84d26
Daniel Haehn <d@klavierfabrik.com>
2012-02-11 01:15:13 -0500

    Some style fix..

--- X.js
+++ X.js
@@ -39,23 +39,28 @@ goog.provide('X');
  */
 var X = X || {};
 
-window["X.Counter"] = (function(){
-  var privateCounter = 0;  
-  function changeBy(val) {  
-    privateCounter += val;  
-  }  
-  return {  
-    increment: function() {  
-      changeBy(1);  
-    },  
-    decrement: function() {  
-      changeBy(-1);  
-    },  
-    value: function() {  
-      return privateCounter;  
-    }  
-  }     
-})();  
+window["X.Counter"] = (function() {
+
+  var privateCounter = 0;
+  function changeBy(val) {
+
+    privateCounter += val;
+  }
+  return {
+    increment: function() {
+
+      changeBy(1);
+    },
+    decrement: function() {
+
+      changeBy(-1);
+    },
+    value: function() {
+
+      return privateCounter;
+    }
+  };
+})();
 
 //
 // BROWSER COMPATIBILITY FIXES GO HERE
