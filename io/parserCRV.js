69d22c1de4d0ae85966607053549b03d3d100221
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-04-10 14:21:53 -0400

    Update X.parserCRV to work without the new X.parserHelper.

--- io/parserCRV.js
+++ io/parserCRV.js
@@ -68,29 +68,31 @@ goog.inherits(X.parserCRV, X.parser);
  */
 X.parserCRV.prototype.parse = function(object, data) {
 
-  dataptr	= new dobj(data, this.parseUInt32EndianSwappedArray, this._sizeof_int);
-  dataptr.b_verbose(false);
-  b1			= dataptr.read();
-  b2			= dataptr.read();
-  b3			= dataptr.read();
+  // dataptr = new dobj(data, this.parseUInt32EndianSwappedArray,
+  // this._sizeof_int);
+  // dataptr.b_verbose(false);
+  // b1 = dataptr.read();
+  // b2 = dataptr.read();
+  // b3 = dataptr.read();
   
-  var vnum		= this.fread3(data);
-		
-  var nvertices		= this.parseUInt32EndianSwapped(data, 3);
-  var nvertices		= dstream.read();
-  var nfaces		= this.parseUInt32EndianSwapped(data, 7);
-  var nvalsPerVertex= this.parseUInt32EndianSwapped(data, 11);
-
-  var af_curvVals 	= [];
-  var al_ret		= [];
-		
-  al_ret 			= this.parseFloat32EndianSwappedArray(data, 15, nvertices);
-  af_curvVals		= al_ret[0];
+  var vnum = this.fread3(data);
   
-  var f_max			= al_ret[1];
-  var f_min			= al_ret[2];
-
-  object.curvVals 	= af_curvVals;
+  var nvertices = this.parseUInt32EndianSwapped(data, 3);
+  
+  // var nvertices = dstream.read();
+  var nfaces = this.parseUInt32EndianSwapped(data, 7);
+  var nvalsPerVertex = this.parseUInt32EndianSwapped(data, 11);
+  
+  var af_curvVals = [];
+  var al_ret = [];
+  
+  al_ret = this.parseFloat32EndianSwappedArray(data, 15, nvertices);
+  af_curvVals = al_ret[0];
+  
+  var f_max = al_ret[1];
+  var f_min = al_ret[2];
+  
+  object.curvVals = af_curvVals;
   
   var modifiedEvent = new X.event.ModifiedEvent();
   modifiedEvent._object = object;
