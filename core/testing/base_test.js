6f995965540dabbe1b84e138fb1503787271891a
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-05-15 11:18:25 -0400

    Fix X.base test.

--- core/testing/base_test.js
+++ core/testing/base_test.js
@@ -9,30 +9,6 @@ function testXbaseClassName() {
 
   b = new X.base();
   
-  assertEquals(b.className(), 'base');
-  
-}
-
-
-/**
- * Test for X.base.print
- */
-function testXbasePrint() {
-
-  b = new X.base();
-  
-  assertContains('== X.base ==\n', b.print());
-  
-}
-
-/**
- * Test for X.base.dirty. Make sure the dirty flag is not set by default.
- */
-function testXbaseDirty() {
-
-  b = new X.base();
-  
-  // object is not dirty by default
-  assertEquals(b.dirty(), false);
+  assertEquals(b.className, 'base');
   
 }
