35205f2c331e1b4596fc3807bf6af9b316ddff92
Daniel Haehn <d@klavierfabrik.com>
2012-06-06 23:03:30 -0400

    WIP, parsing of getters/setters now added.

--- utils/xdoc.py
+++ utils/xdoc.py
@@ -1,3 +1,5 @@
+import pprint
+
 JSDOCSTART = '/**'
 JSDOCEND = '*/'
 GOOGEXPORT = 'goog.exportSymbol('
@@ -7,23 +9,39 @@ PROTOTYPE = 'prototype'
 DEFINESETTER = '__defineSetter__'
 DEFINEGETTER = '__defineGetter__'
 NAMESPACE = 'X'
+TYPES = {'constructor':0, 'static':1, 'function':2, 'getter':3, 'setter':4}
+HIDINGFACTORS = {'private':0, 'public':1}
 
 #
 # Loop through all files and create symbol table
-# [filename, classname, symbols{name,type,jsdoc}, lookup{})
+# [filename, classname, symbols{name,hidingFactor,type,jsdoc}, lookup{})
 # 
 
-with open( '../visualization/renderer3D.js', 'r' ) as f:
+files = {}
+classes = {}
+#files = {'':['', {}, []]}
+#classes = {'classname':{'name': [hidingFactor, type, jsdoc]], []}
 
+filename = 'renderer3D.js'
+
+with open( '../visualization/' + filename, 'r' ) as f:
+
+  # read the whole file
   lines = f.readlines()
 
+# state switches
 jsdocActive = False
 queryIdentifier = False
-tmpBuffer = ''
+jsdocBuffer = ''
 
+# class information
 inherits = ''
 exports = []
-jsdoc = {}
+
+# symbol information
+type = -1
+hidingFactor = 0 # by default private
+
 
 # forward loop through file
 for line in lines:
@@ -44,13 +62,13 @@ for line in lines:
     # check for JSDOC
     if line[0:len( JSDOCSTART )] == JSDOCSTART:
       #found start of JSDOC
-      tmpBuffer += line[0:len( JSDOCSTART )]
+      jsdocBuffer += line[0:len( JSDOCSTART )]
       jsdocActive = True
       continue
 
     if jsdocActive:
       # this is part of the JSDOC
-      tmpBuffer += '\n' + line
+      jsdocBuffer += '\n' + line
 
     if jsdocActive and line[0:len( JSDOCEND )] == JSDOCEND:
       # end of JSDOC
@@ -67,47 +85,98 @@ for line in lines:
         # check if this is a public property
         if line[0:4] == THIS:
 
-          # check if 
+          # check if the property has a constant name aka. is defined with a string
+          if line[4:6] == "['" or line[4:6] == '["':
+            # this is a constant property
+            print 'constant', line
+            continue
 
         else:
           # no namespace so we reset the buffer
-          tmpBuffer = ''
+          jsdocBuffer = ''
           queryIdentifier = False
           continue
 
+        continue
+
       identifier = line.split( ' ' )[0] # split by blank
       identifierSplitted = identifier.split( '.' ) # split by dot
+      #classname = identifierSplitted[1] # should always be the classname      
       identifier = identifierSplitted[-1]
 
       if identifierSplitted[-2] != PROTOTYPE:
 
         # static method or constructor
-        print 'Found static method or constructor: ' + identifier
+        if len( identifierSplitted ) == 2:
+          # this is a constructor
+          type = TYPES['constructor']
+          classname = identifier
+
+        else:
+          # this is a static method
+          type = TYPES['static']
 
       else:
         # a prototype method
+        type = TYPES['function']
 
         # check for getters/setters
         if identifier[0:len( DEFINEGETTER )] == DEFINEGETTER:
           # a getter
           identifier = identifier[len( DEFINEGETTER ):].split( "'" )[1]
+          type = TYPES['getter']
+          hidingFactor = HIDINGFACTORS['public']
 
         elif identifier[0:len( DEFINESETTER )] == DEFINESETTER:
           # a setter
-          identifier = identifier[len( DEFINEGETTER ):].split( "'" )[1]
+          identifier = identifier[len( DEFINESETTER ):].split( "'" )[1]
+          type = TYPES['setter']
+          hidingFactor = HIDINGFACTORS['public']
 
+      if not classes.has_key( classname ):
+        # no symbols for this class yet
+        classes[classname] = {}
 
-      jsdoc[identifier] = tmpBuffer
-      # clear the buffer
-      tmpBuffer = ''
+      # add the current symbol
+      classes[classname][identifier] = [hidingFactor, type, jsdocBuffer]
+
+      # clear the buffer and all other symbol specific data
+      jsdocBuffer = ''
+      type = -1
+      hidingFactor = 0
+      # clear the state switches
       queryIdentifier = False
+
+
+# add to files
+files[filename] = classes
+
+pp = pprint.PrettyPrinter( indent=2 )
 
 print 'Inherits: ', inherits
 
-for e in exports:
-  if jsdoc.has_key( e ):
-    print jsdoc[e]
-    print e
-    print
-  else:
-    print 'could not find ' + e
+
+
+for c in files[filename].keys():
+  # check all detected classes
+  for s in files[filename][c].keys():
+    # check all detected symbols
+
+    for e in exports:
+      # check all detected exports      
+      if e == s:
+        # this is an exported symbol, mark it as public
+        files[filename][c][s][0] = 1
+
+    # check if the symbol is public, else wise discard it
+    if files[filename][c][s][0] != 1:
+      del files[filename][c][s]
+
+
+pp.pprint( files )
+
+#  if jsdoc.has_key( e ):
+#    print jsdoc[e]
+#    print e
+#    print
+##    print 'could not find ' + e
