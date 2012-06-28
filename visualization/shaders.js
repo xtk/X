32fa4ce494e2f5d29233931409a62d3be8942a24
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-06-28 09:39:56 -0400

    Revert "X.shaders fully minified."
    This reverts commit 3cb6d51085901530d348b93c6d5173c283716150.

--- visualization/shaders.js
+++ visualization/shaders.js
@@ -64,33 +64,35 @@ X.shaders = function() {
    * @protected
    */
   this._vertexshaderSource = '';
-  // The uncompiled vertex shader is in the file vertex.shader.
-  // compilation via http://www.ctrl-alt-test.fr/minifier/index
-  //
-  // with the following settings:
-  // GLSL, JS Header, .xyzw, Preserve external values
-  //  
-  var t = 'precision mediump float;'
-      + "attribute vec3 vp,vn,vc;"
-      + "attribute vec2 vtp;"
-      + "attribute float vs;"
-      + "uniform mat4 uv,up,uot;"
-      + "uniform bool uuoc,uus,usrm;"
-      + "uniform vec3 uc,uoc,usminc,usmaxc;"
-      + "uniform float usmin,usmax,ups,usmint,usmaxt;"
-      + "varying float fdn;"
-      + "varying vec4 fvp;"
-      + "varying vec2 ftp;"
-      + "varying vec3 fc,fvn,ftvn;"
+  var t = '';
+  t += 'precision mediump float;'
+      + "attribute vec3 vertexPosition,vertexNormal,vertexColor;"
+      + "attribute vec2 vertexTexturePos;"
+      + "attribute float vertexScalar;"
+      + "uniform mat4 view,perspective,objectTransform;"
+      + "uniform bool useObjectColor,useScalars,scalarsReplaceMode;"
+      + "uniform vec3 center,objectColor,scalarsMinColor,scalarsMaxColor;"
+      + "uniform float scalarsMin,scalarsMax,pointSize,scalarsMinThreshold,scalarsMaxThreshold;"
+      + "varying float fDiscardNow;"
+      + "varying vec4 fVertexPosition;"
+      + "varying vec2 fragmentTexturePos;"
+      + "varying vec3 fragmentColor,fVertexNormal,fTransformedVertexNormal;"
       + "void main()"
       + "{"
-      + "ftvn=mat3(uv[0].xyz,uv[1].xyz,uv[2].xyz)*mat3(uot[0].xyz,uot[1].xyz,uot[2].xyz)*vn;"
-      + "fvn=vn;" + "fdn=0.;" + "vec3 v=vp-uc;" + "fvp=uv*uot*vec4(v,1.);"
-      + "ftp=vtp;" + "if(uus)" + "{" + "float f=vs;" + "if(f<usmint||f>usmaxt)"
-      + "{" + "if(usrm)" + "fc=uoc;" + "else" + " fdn=1.;" + "}" + "else" + "{"
-      + "if(usrm)" + "fc=f*usmaxc+(1.-f)*usminc;" + "else" + " fc=vc;" + "}"
-      + "}" + "else" + " if(uuoc)" + "fc=uoc;" + "else" + " fc=vc;"
-      + "gl_PointSize=ups;" + "gl_Position=up*fvp;" + "}";
+      + "fTransformedVertexNormal=mat3(view[0].xyz,view[1].xyz,view[2].xyz)*mat3(objectTransform[0].xyz,objectTransform[1].xyz,objectTransform[2].xyz)*vertexNormal;"
+      + "fVertexNormal=vertexNormal;" + "fDiscardNow=0.;"
+      + "vec3 v=vertexPosition-center;"
+      + "fVertexPosition=view*objectTransform*vec4(v,1.);"
+      + "fragmentTexturePos=vertexTexturePos;" + "if(useScalars)" + "{"
+      + "float f=vertexScalar;"
+      + "if(f<scalarsMinThreshold||f>scalarsMaxThreshold)" + "{"
+      + "if(scalarsReplaceMode)" + "fragmentColor=objectColor;" + "else"
+      + " fDiscardNow=1.;" + "}" + "else" + "{" + "if(scalarsReplaceMode)"
+      + "fragmentColor=f*scalarsMaxColor+(1.-f)*scalarsMinColor;" + "else"
+      + " fragmentColor=vertexColor;" + "}" + "}" + "else"
+      + " if(useObjectColor)" + "fragmentColor=objectColor;" + "else"
+      + " fragmentColor=vertexColor;" + "gl_PointSize=pointSize;"
+      + "gl_Position=perspective*fVertexPosition;" + "}";
   this._vertexshaderSource = t;
   
   /**
@@ -101,30 +103,65 @@ X.shaders = function() {
    * @protected
    */
   this._fragmentshaderSource = '';
-  // The uncompiled fragment shader is in the file fragment.shader.
+  // The uncompiled fragment shader is in fragment.shader.
   // compilation via http://www.ctrl-alt-test.fr/minifier/index
   //
-  // with the following settings:
-  // GLSL, JS Header, .xyzw, Preserve external values
-  //  
-  var t2 = "precision mediump float;" + "uniform bool uup,uut,uutt,uulmt;"
-      + "uniform sampler2D uts,uts2;"
-      + "uniform float uoo,ulmo,uvlt,uvut,uvsmin,uvsmax;"
-      + "varying float fdn;" + "varying vec4 fvp;"
-      + "varying vec3 fc,fvn,ftvn;" + "varying vec2 ftp;" + "void main()" + "{"
-      + "if(fdn>0.)" + "{" + "discard;" + "}" + "if(uup)"
-      + "gl_FragColor=vec4(fc,1.);" + "else" + " if(uut)" + "{"
-      + "vec4 f=texture2D(uts,ftp),v=f;" + "if(uulmt)" + "{"
-      + "vec4 r=texture2D(uts2,ftp);" + "if(r.w>0.)" + "{" + "if(ulmo<1.)"
-      + "v=mix(r,v,1.-ulmo);" + "else" + " v=r;" + "}" + "}" + "if(uutt)" + "{"
-      + "float r=uvlt/uvsmax,l=uvut/uvsmax;" + "if(f.x<r||f.x>l)" + "{"
-      + "discard;" + "}" + "}" + "gl_FragColor=v;" + "gl_FragColor.w=uoo;"
-      + "}" + "else" + "{" + "vec3 f=normalize(ftvn);"
-      + "if(fvn==vec3(0.,0.,0.))" + "{" + "gl_FragColor=vec4(fc,1.);"
-      + "return;" + "}" + "vec3 v=vec3(0.,0.,1.),r=vec3(0,0,-10);"
-      + "r=normalize(r);" + "vec3 l=normalize(-fvp.xyz),u=reflect(-r,f);"
+  var t2 = "precision mediump float;"
+      + "uniform bool usePicking,useTexture,useTextureThreshold,useLabelMapTexture;"
+      + "uniform sampler2D textureSampler,textureSampler2;"
+      + "uniform float objectOpacity,labelmapOpacity,volumeLowerThreshold,volumeUpperThreshold,volumeScalarMin,volumeScalarMax;"
+      + "varying float fDiscardNow;"
+      + "varying vec4 fVertexPosition;"
+      + "varying vec3 fragmentColor,fVertexNormal,fTransformedVertexNormal;"
+      + "varying vec2 fragmentTexturePos;"
+      + "void main()"
+      + "{"
+      + "if(fDiscardNow>0.)"
+      + "{"
+      + "discard;"
+      + "}"
+      + "if(usePicking)"
+      + "gl_FragColor=vec4(fragmentColor,1.);"
+      + "else"
+      + " if(useTexture)"
+      + "{"
+      + "vec4 f=texture2D(textureSampler,fragmentTexturePos),v=f;"
+      + "if(useLabelMapTexture)"
+      + "{"
+      + "vec4 r=texture2D(textureSampler2,fragmentTexturePos);"
+      + "if(r.w>0.)"
+      + "{"
+      + "if(labelmapOpacity<1.)"
+      + "v=mix(r,v,1.-labelmapOpacity);"
+      + "else"
+      + " v=r;"
+      + "}"
+      + "}"
+      + "if(useTextureThreshold)"
+      + "{"
+      + "float r=volumeLowerThreshold/volumeScalarMax,l=volumeUpperThreshold/volumeScalarMax;"
+      + "if(f.x<r||f.x>l)"
+      + "{"
+      + "discard;"
+      + "}"
+      + "}"
+      + "gl_FragColor=v;"
+      + "gl_FragColor.w=objectOpacity;"
+      + "}"
+      + "else"
+      + "{"
+      + "vec3 f=normalize(fTransformedVertexNormal);"
+      + "if(fVertexNormal==vec3(0.,0.,0.))"
+      + "{"
+      + "gl_FragColor=vec4(fragmentColor,1.);"
+      + "return;"
+      + "}"
+      + "vec3 v=vec3(0.,0.,1.),r=vec3(0,0,-10);"
+      + "r=normalize(r);"
+      + "vec3 l=normalize(-fVertexPosition.xyz),u=reflect(-r,f);"
       + "float d=pow(max(dot(u,l),0.),10.),o=.8*max(dot(f,v),0.),t=.3;"
-      + "gl_FragColor=vec4(fc*t+fc*o+vec3(.2,.2,.2)*d,uoo);" + "}" + "}";
+      + "gl_FragColor=vec4(fragmentColor*t+fragmentColor*o+vec3(.2,.2,.2)*d,objectOpacity);"
+      + "}" + "}";
   this._fragmentshaderSource = t2;
   
 };
@@ -139,11 +176,11 @@ goog.inherits(X.shaders, X.base);
  * @protected
  */
 X.shaders.attributes = {
-  VERTEXPOSITION: 'vp',
-  VERTEXNORMAL: 'vn',
-  VERTEXCOLOR: 'vc',
-  VERTEXTEXTUREPOS: 'vtp',
-  VERTEXSCALAR: 'vs'
+  VERTEXPOSITION: 'vertexPosition',
+  VERTEXNORMAL: 'vertexNormal',
+  VERTEXCOLOR: 'vertexColor',
+  VERTEXTEXTUREPOS: 'vertexTexturePos',
+  VERTEXSCALAR: 'vertexScalar'
 };
 
 
@@ -154,34 +191,34 @@ X.shaders.attributes = {
  * @protected
  */
 X.shaders.uniforms = {
-  VIEW: 'uv',
-  PERSPECTIVE: 'up',
-  CENTER: 'uc',
-  OBJECTTRANSFORM: 'uot',
-  USEOBJECTCOLOR: 'uuoc',
-  OBJECTCOLOR: 'uoc',
-  USESCALARS: 'uus',
-  SCALARSREPLACEMODE: 'usrm',
-  SCALARSMIN: 'usmin',
-  SCALARSMAX: 'usmax',
-  SCALARSMINCOLOR: 'usminc',
-  SCALARSMAXCOLOR: 'usmaxc',
-  SCALARSMINTHRESHOLD: 'usmint',
-  SCALARSMAXTHRESHOLD: 'usmaxt',
-  POINTSIZE: 'ups',
-  OBJECTOPACITY: 'uoo',
-  NORMAL: 'un',
-  USEPICKING: 'uup',
-  USETEXTURE: 'uut',
-  USETEXTURETHRESHOLD: 'uutt',
-  USELABELMAPTEXTURE: 'uulmt',
-  LABELMAPOPACITY: 'ulmo',
-  TEXTURESAMPLER: 'uts',
-  TEXTURESAMPLER2: 'uts2',
-  VOLUMELOWERTHRESHOLD: 'uvlt',
-  VOLUMEUPPERTHRESHOLD: 'uvut',
-  VOLUMESCALARMIN: 'uvsmin',
-  VOLUMESCALARMAX: 'uvsmax'
+  VIEW: 'view',
+  PERSPECTIVE: 'perspective',
+  CENTER: 'center',
+  OBJECTTRANSFORM: 'objectTransform',
+  USEOBJECTCOLOR: 'useObjectColor',
+  OBJECTCOLOR: 'objectColor',
+  USESCALARS: 'useScalars',
+  SCALARSREPLACEMODE: 'scalarsReplaceMode',
+  SCALARSMIN: 'scalarsMin',
+  SCALARSMAX: 'scalarsMax',
+  SCALARSMINCOLOR: 'scalarsMinColor',
+  SCALARSMAXCOLOR: 'scalarsMaxColor',
+  SCALARSMINTHRESHOLD: 'scalarsMinThreshold',
+  SCALARSMAXTHRESHOLD: 'scalarsMaxThreshold',
+  POINTSIZE: 'pointSize',
+  OBJECTOPACITY: 'objectOpacity',
+  NORMAL: 'normal',
+  USEPICKING: 'usePicking',
+  USETEXTURE: 'useTexture',
+  USETEXTURETHRESHOLD: 'useTextureThreshold',
+  USELABELMAPTEXTURE: 'useLabelMapTexture',
+  LABELMAPOPACITY: 'labelmapOpacity',
+  TEXTURESAMPLER: 'textureSampler',
+  TEXTURESAMPLER2: 'textureSampler2',
+  VOLUMELOWERTHRESHOLD: 'volumeLowerThreshold',
+  VOLUMEUPPERTHRESHOLD: 'volumeUpperThreshold',
+  VOLUMESCALARMIN: 'volumeScalarMin',
+  VOLUMESCALARMAX: 'volumeScalarMax'
 };
 
 
