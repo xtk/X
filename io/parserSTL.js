f1d836baf5f6d6939ea86b64be5e78f7902781c5
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-08-08 12:03:29 -0400

    Fix bug in parsing ASCII STL files.
    closes #81

--- io/parserSTL.js
+++ io/parserSTL.js
@@ -46,16 +46,16 @@ X.parserSTL = function() {
   //
   // call the standard constructor of X.base
   goog.base(this);
-
+  
   //
   // class attributes
-
+  
   /**
    * @inheritDoc
    * @const
    */
   this._classname = 'parserSTL';
-
+  
 };
 // inherit from X.parser
 goog.inherits(X.parserSTL, X.parser);
@@ -73,43 +73,47 @@ X.parserSTL.prototype.parse = function(container, object, data, flag) {
   var _parseFunction = null;
   
   // check if this is an ascii STL file or a binary one
-  if ( data.substr(0, 5) == 'solid' ) {
-
+  if (data.substr(0, 5) == 'solid') {
+    
     // this is an ascii STL file
-
+    
+    // split the data
+    data = data.split('\n');
+    
     // get the number of lines
-    _size = data.split('\n').length;
-
+    _size = data.length;
+    
     // set the parse function for ASCII
     _parseFunction = this.parseLine.bind(this);
-
+    
   } else {
-
-    // this is a binary STL file (http://en.wikipedia.org/wiki/STL_(file_format))
-
+    
+    // this is a binary STL file
+    // (http://en.wikipedia.org/wiki/STL_(file_format))
+    
     // A binary STL file has an 80 character header (which is generally
     // ignored, but which should never begin with 'solid' because that will
     // lead most software to assume that this is an ASCII STL file).
     var header_data = data.slice(0, 80);
-
+    
     // Following the header is a 4 byte unsigned integer indicating
     // the number of triangular facets in the file.
     _size = this.parseUInt32(data, 80);
-
+    
     // set the parse function for binary
     _parseFunction = this.parseBytes.bind(this);
-
+    
   }
-
+  
   var p = object._points;
   var n = object._normals;
-
+  
   //
   // THE LOOP
   //
   // This uses an optimized loop.
   //
-
+  
   //
   // This one is shorter but Fast Duff's Device is slightly faster on average.
   //
@@ -120,7 +124,7 @@ X.parserSTL.prototype.parse = function(container, object, data, flag) {
   // this.parseLine_(p, n, dataAsArray[i]);
   //    
   // } while (i > 0);
-
+  
   /*
    * Fast Duff's Device @author Miller Medeiros <http://millermedeiros.com>
    * 
@@ -132,7 +136,7 @@ X.parserSTL.prototype.parse = function(container, object, data, flag) {
     _parseFunction(p, n, data, i);
     i++;
   }
-
+  
   n2 = (_size * 0.125) ^ 0;
   while (n2--) {
     _parseFunction(p, n, data, i);
@@ -152,13 +156,13 @@ X.parserSTL.prototype.parse = function(container, object, data, flag) {
     _parseFunction(p, n, data, i);
     i++;
   }
-
+  
   // the object should be set up here, so let's fire a modified event
   var modifiedEvent = new X.event.ModifiedEvent();
   modifiedEvent._object = object;
   modifiedEvent._container = container;
   this.dispatchEvent(modifiedEvent);
-
+  
 };
 
 
@@ -175,23 +179,23 @@ X.parserSTL.prototype.parseLine = function(p, n, data, index) {
 
   // grab the current line
   var _data = data[index];
-
+  
   // trim the line
   var line = _data.replace(/^\s+|\s+$/g, '');
-
+  
   // split to array
   var lineFields = line.split(' ');
-
-  if ( lineFields[0] == 'vertex' ) {
-
+  
+  if (lineFields[0] == 'vertex') {
+    
     // add point
     var x = parseFloat(lineFields[1]);
     var y = parseFloat(lineFields[2]);
     var z = parseFloat(lineFields[3]);
     p.add(x, y, z);
-
-  } else if ( lineFields[0] == 'facet' ) {
-
+    
+  } else if (lineFields[0] == 'facet') {
+    
     // add normals
     var x = parseFloat(lineFields[2]);
     var y = parseFloat(lineFields[3]);
@@ -199,9 +203,9 @@ X.parserSTL.prototype.parseLine = function(p, n, data, index) {
     n.add(x, y, z);
     n.add(x, y, z);
     n.add(x, y, z);
-
+    
   }
-
+  
 };
 
 
@@ -225,37 +229,37 @@ X.parserSTL.prototype.parseBytes = function(p, n, data, index) {
   // REAL32[3] â Normal vector
   var normal = this.parseFloat32Array(data, offset, 3)[0];
   offset += 3 * 4;
-
+  
   // REAL32[3] â Vertex 1
   var v1 = this.parseFloat32Array(data, offset, 3)[0];
   offset += 3 * 4;
-
+  
   // REAL32[3] â Vertex 2
   var v2 = this.parseFloat32Array(data, offset, 3)[0];
   offset += 3 * 4;
-
+  
   // REAL32[3] â Vertex 3
   var v3 = this.parseFloat32Array(data, offset, 3)[0];
   offset += 3 * 4;
-
+  
   // MRG TODO:
   // The above could probably be made faster by
   // making a single read of 12 float32 values.
-
+  
   // UINT16 â Attribute byte count
   var attributes = this.parseUInt16(data, offset)[0];
   offset += 2;
-
+  
   // Add the vertices
   p.add(v1[0], v1[1], v1[2]);
   p.add(v2[0], v2[1], v2[2]);
   p.add(v3[0], v3[1], v3[2]);
-
+  
   // Add the Normals
   n.add(normal[0], normal[1], normal[2]);
   n.add(normal[0], normal[1], normal[2]);
   n.add(normal[0], normal[1], normal[2]);
-
+  
 };
 
 // export symbols (required for advanced compilation)
