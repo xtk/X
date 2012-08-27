a2375434ab9c6ae098545915ad9027d17e67f2aa
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-08-27 14:27:42 -0400

    Incorporate the new measurements in all tests.

--- testing/visualization/js/test_mgz.js
+++ testing/visualization/js/test_mgz.js
@@ -1,24 +1,5 @@
-if (location.href.match(/(\?|&)build($|&|=)/)) {
-  
-  // testing the BUILD tree
-  addJavascript('../../utils/xtk.js', 'head');
-  
-  console.log('Testing the BUILD tree.');
-  
-} else {
-  
-  console.log('Testing the DEV tree.');
-  
-  // includes
-  goog.require('X.renderer3D');
-  goog.require('X.volume');
-  
-}
+test = function() {
 
-window.onload = function() {
-
-  window.console.time('startup');
-  
   // create a new test_renderer
   test_renderer = new X.renderer3D();
   test_renderer.init();
@@ -35,11 +16,5 @@ window.onload = function() {
   
   // .. and render it
   test_renderer.render();
-  
-  test_renderer.onShowtime = function() {
-
-    window.console.timeEnd('startup');
-    
-  };
   
 };
