4fe8db5643f4633891803ca4c3342fe2ec94a96c
robin <robinnagpal.tiet@gmail.com>
2014-07-22 14:15:33 -0400

    restore the unnecessary changes

--- visualization/renderer.js
+++ visualization/renderer.js
@@ -852,20 +852,7 @@ X.renderer.prototype.remove = function(object) {
 
   }
   else{
-
-    goog.events.removeAll(object);
-
-    var _numberOfTopLevelObjects = this._topLevelObjects.length;
-
-    var _y;
-    for (_y = 0; _y < _numberOfTopLevelObjects; _y++) {
-
-      if(this._topLevelObjects[_y]._id == object._id){
-        this._topLevelObjects[_y] = null;
-        this._topLevelObjects.splice(_y, 1);
-        return true;
-      }
-    }
+      goog.events.removeAll(object);
   }
 
 	// to be overloaded
@@ -987,8 +974,6 @@ X.renderer.prototype.get = function(id) {
 X.renderer.prototype.printScene = function() {
 
   var _numberOfTopLevelObjects = this._topLevelObjects.length;
-  // window.console.log(_numberOfTopLevelObjects);
-  // window.console.log(this._objects);
 
   var _y;
   for (_y = 0; _y < _numberOfTopLevelObjects; _y++) {
@@ -1011,11 +996,6 @@ X.renderer.prototype.printScene = function() {
  */
 X.renderer.prototype.generateTree_ = function(object, level) {
 
-  // for slices, container is right size but empty
-  if(typeof(object) == 'undefined'){
-    return;
-  }
-
   var _output = "";
 
   var _l = 0;
@@ -1027,8 +1007,7 @@ X.renderer.prototype.generateTree_ = function(object, level) {
 
   _output += object._id;
 
-  // window.console.log(object);
-  // window.console.log(_output);
+  window.console.log(_output);
 
   if (object._children.length > 0) {
 
@@ -1081,6 +1060,8 @@ X.renderer.prototype.render = function() {
     return;
 
   }
+
+
 
   //
   // LOADING..
@@ -1220,11 +1201,6 @@ X.renderer.prototype.render_ = function(picking, invoked) {
  * @public
  */
 X.renderer.prototype.destroy = function() {
-
-  // disconnect events listeners
-  goog.events.removeAll(this);
-  goog.events.unlisten(window, goog.events.EventType.RESIZE, this.onResize_,
-      false, this);
 
   // stop the rendering loop
   window.cancelAnimationFrame(this._AnimationFrameID);
